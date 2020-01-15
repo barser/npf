011f419fccb5fad2ff17c27f3247496c647dc01b
Serg Bara <baraserg@gmail.com>
2020-01-15 06:29:41 +0300

    Pocard's OfficeAttachmentMetadata prototype added.

--- npf-pocard/src/main/java/ru/ospos/npf/officeaddin/domain/OfficeAttachmentMetadata.java
+++ npf-pocard/src/main/java/ru/ospos/npf/officeaddin/domain/OfficeAttachmentMetadata.java
@@ -1,21 +1,44 @@
 package ru.ospos.npf.officeaddin.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 import ru.ospos.npf.commons.domain.base.FileStorage;
 import ru.ospos.npf.commons.domain.document.Pocard;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 /**
  * Файл, передаваемый из надстройки Microsoft Office.
  */
+@Getter
+@Setter
+@Entity
+@Table(name = "POCARD_OFFICE_ATTACH_META")
 public class OfficeAttachmentMetadata implements Serializable {
 
     private static final long serialVersionUID = -4527277548020979941L;
 
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Id
+    private UUID id;
+
+    @Version
+    private int version;
+
+    @ManyToOne
+    @JoinColumn(name = "POCARD_ID", nullable = false)
     private Pocard pocard;
+
+    @ManyToOne
+    @JoinColumn(name = "FILESTORAGE_ID", nullable = false)
     private FileStorage fileStorage;
-    private String checkSum;
+
+    private Long checkSum;
     private Long size;
     private String originPath;
     private LocalDateTime creationTs;
